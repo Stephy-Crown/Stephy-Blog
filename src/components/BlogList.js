ffc486b249336663516c500a9a6e4503d1deae49
Stephanie Udemezue <udemezuestephanie6@gmail.com>
2024-10-17 21:54:48 +0100

    add bloglist to the homepage

---
+++ src/components/BlogList.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const BlogList = ({ blogs, title, handleDelete }) => {
+  return (
+    <div>
+      {" "}
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <button
+            onClick={() => {
+              handleDelete(blog.id);
+            }}
+          >
+            Delete
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
